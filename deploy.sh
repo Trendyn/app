3c7249c88affb92428f0eee534c55760fc957fe9
System Admin <admin@opynios.com>
2015-11-24 23:58:15 -0800

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ fi
 
 curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-docker-compose -p $OP_USER --x-networking build
+docker-compose -p $OP_USER build
 docker tag "${OP_USER}_app" "tutum.co/opynios/app:${OP_USER}"
 docker tag "${OP_USER}_db" "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/db:${OP_USER}"
