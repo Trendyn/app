5e5a8d971b9acf01a881246ab74a5de7df8942b3
System Admin <admin@opynios.com>
2015-11-25 12:14:57 -0800

    Fixed OP_USER hyphen issue, because docker-compose remove's hyphen from project name. Thus script need to account for that to lookup generated images.

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ else
 fi
 
 docker-compose -p $OP_USER build
-docker tag "${OP_USER}_app" "tutum.co/opynios/app:${OP_USER}"
-docker tag "${OP_USER}_db" "tutum.co/opynios/db:${OP_USER}"
+docker tag "${OP_USER//-}_app" "tutum.co/opynios/app:${OP_USER}"
+docker tag "${OP_USER//-}_db" "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/app:${OP_USER}"
 ./replacevars.sh tutum-staging.yml OP_USER=${OP_USER}
