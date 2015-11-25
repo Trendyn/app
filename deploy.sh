8e6ff2214cdb1b133cdaa7d18a4bcab53440d94e
System Admin <admin@opynios.com>
2015-11-25 02:58:59 -0800

    Fixed tutum stack create failure

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ docker tag "${OP_USER}_app" "tutum.co/opynios/app:${OP_USER}"
 docker tag "${OP_USER}_db" "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/app:${OP_USER}"
-tutum stack create -f tutum.yml -n "staging_${OP_USER}"
-tutum stack start "staging_${OP_USER}"
+tutum stack create -f tutum.yml -n "staging-${OP_USER}"
+tutum stack start "staging-${OP_USER}"
 
 
 
