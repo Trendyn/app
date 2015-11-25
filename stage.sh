b2f47400f6a81395ad3154e675ef6a0af4aca434
System Admin <admin@opynios.com>
2015-11-25 03:30:19 -0800

    Added per user staging support

--- stage.sh
+++ stage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ssh -i ~/.ssh/tutum  root@$HOST "eval \$(ssh-agent -s) && ssh-add ~/.ssh/github && rm -rf app &&  git clone $1 && cd app/ && ./deploy.sh"
+ssh -i ~/.ssh/tutum  root@$HOST "eval \$(ssh-agent -s) && ssh-add ~/.ssh/github && mkdir -p ${OP_USER} && cd ${OP_USER} && rm -rf app && git clone $1 && cd app/ && ./deploy.sh"
 
 
