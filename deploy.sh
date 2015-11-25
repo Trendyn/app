7a6804e94a447581f15c94fa3f37d8e5f8805d39
System Admin <admin@opynios.com>
2015-11-25 03:10:03 -0800

    Adding variable support in tutum.yml

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ docker tag "${OP_USER}_app" "tutum.co/opynios/app:${OP_USER}"
 docker tag "${OP_USER}_db" "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/app:${OP_USER}"
-tutum stack create -f tutum.yml -n "staging-${OP_USER}"
+./replacevars tutum-staging.yml OP_USER=$OP_USER
+tutum stack create -f tutum-staging.yml -n "staging-${OP_USER}"
 tutum stack start "staging-${OP_USER}"
 
 
