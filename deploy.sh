0326fc313f26995d18e2ae67a04fd553a1b9628e
System Admin <admin@opynios.com>
2015-11-25 03:20:19 -0800

    Staging deploy works!!

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ docker tag "${OP_USER}_app" "tutum.co/opynios/app:${OP_USER}"
 docker tag "${OP_USER}_db" "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/app:${OP_USER}"
-./replacevars tutum-staging.yml OP_USER=$OP_USER
+./replacevars.sh tutum-staging.yml OP_USER=${OP_USER}
 tutum stack create -f tutum-staging.yml -n "staging-${OP_USER}"
 tutum stack start "staging-${OP_USER}"
 
