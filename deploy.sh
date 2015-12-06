696af1dca77f4a03ee52896fb1d2ae247c0050e8
System Admin <admin@opynios.com>
2015-12-05 19:41:20 -0800

    Localizing docker-compose bin file to work with jenkins

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ fi
 if which docker-compose > /dev/null; then
   echo "docker-compose found"
 else
-  curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > ./bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 fi
 
-docker-compose -p $OP_USER build
+./bin/docker-compose -p $OP_USER build
 docker tag "${OP_USER//-}_app" "tutum.co/opynios/app:${OP_USER}"
 docker tag "${OP_USER//-}_db" "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/db:${OP_USER}"
