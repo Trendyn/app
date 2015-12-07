43d65bedb02fae722282dc0a5de7a7b523fc9fa5
System Admin <admin@opynios.com>
2015-12-06 19:27:06 -0800

    Added --sync option to tutum commands

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ docker tag "${OP_USER//-}_db" "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/db:${OP_USER}"
 docker push "tutum.co/opynios/app:${OP_USER}"
 ./replacevars.sh tutum-staging.yml OP_USER=${OP_USER}
-tutum stack create -f tutum-staging.yml -n "staging-${OP_USER}"
-tutum stack start "staging-${OP_USER}"
+tutum stack create -f tutum-staging.yml -n "staging-${OP_USER}" --sync
+tutum stack start "staging-${OP_USER}" --sync
 
 
 
